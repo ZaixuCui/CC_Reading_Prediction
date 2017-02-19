d3a3e930857cc7941d502ca8f1a2e0c4412d1b84
ZaixuCui <zaixucui@gmail.com>
2017-02-19 20:53:59 +0800

    Python code for permutation test of S500 PVT model predict New400 dataset

---
+++ PVT_ElasticNet_S500_Predict_New400_Permutation.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 20:39:09 2015
+
+@author: Cui
+"""
+
+import scipy.io as sio
+import numpy as np
+import sys
+sys.path.append('/lustre/gaolab/cuizaixu/Utilities_Zaixu/Utilities_Regression/ElasticNet')
+import CZ_ElasticNet_Revise
+
+ParentFolder = '/lustre/gaolab/cuizaixu/DATA_HCP_Reading_Revise/Prediction_S500_S900New_S2/PicVocab_AgeAdj_All'
+
+data = sio.loadmat(ParentFolder + '/PicVocab_AgeAdj_S500_All.mat')
+PicVocab_AgeAdj_S500_All = data['PicVocab_AgeAdj_S500_All']
+PicVocab_AgeAdj_S500_All = np.transpose(PicVocab_AgeAdj_S500_All)
+PicVocab_AgeAdj_S500_All = PicVocab_AgeAdj_S500_All[0]
+data = sio.loadmat(ParentFolder + '/PicVocab_AgeAdj_S900New_All.mat')
+PicVocab_AgeAdj_S900New_All = data['PicVocab_AgeAdj_S900New_All']
+PicVocab_AgeAdj_S900New_All = np.transpose(PicVocab_AgeAdj_S900New_All)
+PicVocab_AgeAdj_S900New_All = PicVocab_AgeAdj_S900New_All[0]
+
+GMV_S500_All_training_mat = sio.loadmat(ParentFolder + '/GMV_S500_All_training.mat')
+GMV_S500_All_training = GMV_S500_All_training_mat['GMV_S500_All_training']
+GMV_S900New_All_testing_mat = sio.loadmat(ParentFolder + '/GMV_S900New_All_testing.mat')
+GMV_S900New_All_testing = GMV_S900New_All_testing_mat['GMV_S900New_All_testing']
+
+CZ_ElasticNet_Revise.ElasticNet_APredictB_Permutation(GMV_S500_All_training, PicVocab_AgeAdj_S500_All, GMV_S900New_All_testing, \
+    PicVocab_AgeAdj_S900New_All, np.arange(1000), np.exp(np.linspace(-6,5,20)), np.linspace(0.2, 1, 10), 3, ParentFolder + '/Permutation2', 1, 600, '-q short -l nodes=1:ppn=1')     
+    
+    
+    
+        
+    
+
+
+
+
+
+
+
